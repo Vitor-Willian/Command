78a2d6e4418c352864bee6be19b00895cd3c00c1
MarcosUeda42 <m.ueda@unifesp.br>
2026-06-23 16:58:12 -0300

    ADD: implementação ADD

--- BancoDados/src/New.java
+++ BancoDados/src/New.java
@@ -1,13 +1,19 @@
 public class New implements Command {
 
-    /*
-    public Object execute(Object args) {
-        Data data = (Data) args;
-        int id = data.getArg(0);
-        String nome = data.getArg(1);
-        Pessoa pessoa = new Pessoa(id, nome);
-        return pessoa;
-    }
-    */
+    public Object execute(Object... args) throws Exception {
+        
+        Object[] objs = (Object[]) args;
+        Object banco = objs[0];
+        Object pessoa = objs[1];
+        if (banco instanceof BancoPessoas && pessoa instanceof Pessoa) {
+            BancoPessoas b = (BancoPessoas) banco;
+            Pessoa p = (Pessoa) pessoa;
+            b.getPessoas().put(p.getId(), p);
+            return true;
+        }else {
+            throw new Exception("Argumentos inválidos para o comando New");
+        }
+    }   
+    
     
 }
