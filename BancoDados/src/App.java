02d237c0b011d7ae1c6002da3dd8f45214929bb4
MarcosUeda42 <m.ueda@unifesp.br>
2026-06-23 17:15:16 -0300

    ADD: testes na main

--- BancoDados/src/App.java
+++ BancoDados/src/App.java
@@ -3,6 +3,9 @@ public class App {
         BancoPessoas banco = new BancoPessoas();
         
         banco.service("new", banco, 1, "João");
+        banco.service("new", banco, 2, "Marcos");
+        banco.service("get", banco, 1);
+        banco.service("getall", banco);
 
     }
 }
