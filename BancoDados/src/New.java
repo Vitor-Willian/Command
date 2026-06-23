6a83003f925ab20c2d76c3653d93ef5bc3c1adc7
MarcosUeda42 <m.ueda@unifesp.br>
2026-06-23 16:45:30 -0300

    ADD: comando New

---
+++ BancoDados/src/New.java
@@ -0,0 +1,13 @@
+public class New implements Command {
+
+    /*
+    public Object execute(Object args) {
+        Data data = (Data) args;
+        int id = data.getArg(0);
+        String nome = data.getArg(1);
+        Pessoa pessoa = new Pessoa(id, nome);
+        return pessoa;
+    }
+    */
+    
+}
